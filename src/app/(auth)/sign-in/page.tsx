06589984df0c99f660b2cb3292e0bde0687a3aa2
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-08-24 15:36:08 +0530

    initialized git

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button className="bg-orange-400 rounded-2xl py-1.5 px-4 my-2 mx-3" onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
