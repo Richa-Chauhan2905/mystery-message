b567f75966941251ed978eb7bbe400a6902fd128
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-08-24 21:54:17 +0530

    sign-up backend working

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,25 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Component() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    )
+    );
   }
   return (
     <>
       Not signed in <br />
-      <button className="bg-orange-400 rounded-2xl py-1.5 px-4 my-2 mx-3" onClick={() => signIn()}>Sign in</button>
+      <button
+        className="bg-orange-400 rounded-2xl py-1.5 px-4 my-2 mx-3"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </>
-  )
+  );
 }
